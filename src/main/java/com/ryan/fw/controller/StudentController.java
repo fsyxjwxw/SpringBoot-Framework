df90f6831b25f836d18a8644ce37478a56b16ffc
Gogs <gogs@fake.local>
2022-07-29 09:33:57 +0800

    整合swagger简单案例

--- src/main/java/com/ryan/fw/controller/StudentController.java
+++ src/main/java/com/ryan/fw/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.ryan.fw.controller;
 
 import com.ryan.fw.entity.co.Result;
 import com.ryan.fw.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,14 @@ import javax.annotation.Resource;
  * @date 2022/7/27 12:02
  * @description
  */
+@Api(tags = "【学生操作】")
 @RestController
 public class StudentController {
 
     @Resource
     private StudentService studentService;
 
+    @ApiOperation("根据id查询学生信息")
     @GetMapping("/getStudentById")
     public Result getStudentById(@RequestParam("id") String id) {
         return Result.success("查询成功", studentService.one(id));
